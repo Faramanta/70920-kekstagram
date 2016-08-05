c30a547a623dd203ea1be8d7ab257dbb3693a2ca
Faramanta <katuxa@yandex.ru>
2016-08-05 14:59:31 +0500

    module1-task2, убрала лишнюю проверку массива

--- build/js/check.js
+++ build/js/check.js
@@ -12,20 +12,20 @@ function getMessage(a, b) {
 		return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов');
 	}
 
-  if (Array.isArray(a)&&!Array.isArray(b)) {
-		var amountOfRedPoints = a.reduce(function(prev, current){
-			return prev + current;
-		},0);
-		return ('Количество красных точек во всех строчках изображения: ' + amountOfRedPoints);
-	}
-
-  if (Array.isArray(a)&&Array.isArray(b)){
-		var artifactsSquare = 0;
-		for (var i=0; i<a.length; i++){
-			artifactsSquare += a[i] * b[i];
-		}
-		return ('Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей');
-	}
+  if (Array.isArray(a)){
+    if (Array.isArray(b)){
+      var artifactsSquare = 0;
+      for (var i=0; i<a.length; i++){
+        artifactsSquare += a[i] * b[i];
+      }
+      return ('Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей');
+    } else {
+      var amountOfRedPoints = a.reduce(function(prev, current){
+        return prev + current;
+      },0);
+      return ('Количество красных точек во всех строчках изображения: ' + amountOfRedPoints);
+    }
+  }
 }
 
 
